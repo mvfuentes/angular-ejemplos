571e3b1c1d202cb34692a860429908a4eb9d36ef
mvfuentes <mvfuentes@gmail.com>
2021-07-05 15:58:06 -0600

    4 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
-import {Product, products } from '../products';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {Product, products } from '../products';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-
+  product: Product|undefined;
   constructor(
     private route: ActivatedRoute,
   ) { }
@@ -17,7 +18,7 @@ export class ProductDetailsComponent implements OnInit {
   const productIdFromRoute = Number(routeParams.get('productId'));
 
   // Find the product that correspond with the id provided in route.
-  //this.product = products.find(product => product.id === productIdFromRoute);
+  this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
